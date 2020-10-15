052ffacf930da5d58124cc2ba6d41352d78072b7
will-ing <will-ing@github.com>
2020-10-14 18:51:01 -0700

    added more to buildout

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Steps
 
 1. poetry init -n
 2. poetry shell
-3. poetry add django
+3. poetry add django djangorestframework_simplejwt django-cors-headers whitenoise gunicorn psycopg2-binary djangorestframework
 4. django-admin startproject "project name"
 5. python manage.py startapp "app name"
+6. fix settings.py
+7. add files urls.py, permissions.py, serializers.py
--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer
+from rest_framework_simplejwt import views as jwt_views
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
